41a94306cb837ec8aa3d0c9b02ef5ca08e143422
Sumair <sumairqaisar992@gmail.com>
2022-06-20 18:57:40 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'static/index'
+  namespace :api, defaults: { format: 'json' } do
+    get 'greeting', to: 'greetings#index'
+  end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    namespace :v1, defaults: { format: 'json' } do
-      get 'greetings', to: 'greetings#index'
-    end 
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
